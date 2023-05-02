37bfb4b4229abf2ee31043cd8db70a9b384ddcd9
leoli-git <jafee201153@gmail.com>
2023-05-02 21:31:15 +0800

    feat: add test for mint, batchMint

--- test/Quiz_02.js
+++ test/Quiz_02.js
@@ -26,4 +26,28 @@ describe("Quiz_02", function () {
   it("should support interface", async function () {
     expect(await token.supportsInterface("0x00000001")).to.equal(false);
   });
+
+  // reference: https://github.com/z-institute/Quiz/blob/main/test/Quiz_02.test.js
+  it("should be able to mint to specified address", async function () {
+    await token.mint(owner.address, 0, {
+      value: ethers.utils.parseEther("0.1"),
+    });
+    expect(await token.ownerOf(0)).to.equal(owner.address);
+
+    await token.batchMint(addr1.address, [1, 2], {
+      value: ethers.utils.parseEther("0.2"),
+    });
+    expect(await token.ownerOf(1)).to.equal(addr1.address);
+    expect(await token.ownerOf(2)).to.equal(addr2.address);
+  });
+
+  it("should revert when not enough funds", async function () {
+    await expect(
+      token.mint(owner.address, 0)
+    ).to.be.revertedWith("Not enough funds to mint.");
+
+    await expect(
+      token.batchMint(addr1.address, [0, 1])
+    ).to.be.revertedWith("Not enough funds to mint.");
+  });
 });
