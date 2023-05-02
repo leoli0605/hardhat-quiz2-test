6d7daf8d98d9762c0b956e16d5a3754416ef94a4
leoli-git <jafee201153@gmail.com>
2023-05-02 21:05:44 +0800

    fix: modify the contract name to the correct one

--- test/Quiz_02.js
+++ test/Quiz_02.js
@@ -5,8 +5,8 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-let Quiz_02;
-let quiz_02;
+let MyToken;
+let token;
 let owner;
 let addr1;
 let addr2;
@@ -14,7 +14,7 @@ let addr2;
 describe("Quiz_02", function () {
   this.beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners(); // 如何知道會有幾個 address?
-    Quiz_02 = await ethers.getContractFactory("Quiz_02");
-    quiz_02 = await Quiz_02.deploy();
+    MyToken = await ethers.getContractFactory("MyToken");
+    token = await MyToken.deploy();
   });
 });
